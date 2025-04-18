e0da5df5ccf0175032b5aad673ab7fc9e2818fbd
Coop-j <jc2056@sussex.ac.uk>
2025-04-18 12:38:37 +0100

    Ver1

--- webapp2025/urls.py
+++ webapp2025/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.shortcuts import redirect
 from django.urls import path, include
 from payapp import views
 
 urlpatterns = [
+    path('', lambda request: redirect('register/')),
     path('admin/', admin.site.urls),
     path('', include('register.urls')),
     path('payments/', include('payapp.urls')),

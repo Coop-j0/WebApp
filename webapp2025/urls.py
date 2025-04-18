e5360b97f5c611e7c1e24fbedb7167617d630621
Coop-j0 <j-r-cooper@outlook.com>
2025-04-18 18:00:53 +0100

    Portfolio page

--- webapp2025/urls.py
+++ webapp2025/urls.py
@@ -20,7 +20,7 @@ from django.urls import path, include
 from payapp import views
 
 urlpatterns = [
-    path('', lambda request: redirect('portfolio_page/')),
+    path('', lambda request: redirect('payments/portfolio_page/')),
     path('admin/', admin.site.urls),
     path('', include('register.urls')),
     path('payments/', include('payapp.urls')),
